f8ce410d39f39a077c8cfe344250269a4b1684c6
Matias Kochman <matiaskochman@gmail.com>
2025-02-19 12:15:03 -0300

    Initial commit

---
+++ test/minibus-backend-aws-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MinibusBackendAwsCdk from '../lib/minibus-backend-aws-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/minibus-backend-aws-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MinibusBackendAwsCdk.MinibusBackendAwsCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
